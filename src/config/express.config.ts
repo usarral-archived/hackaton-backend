59bc1242413ddd5048474fdf1d6d8a9571e3fd02
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-04-13 20:19:33 +0000

    Add default route

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import { TYPE } from '@config/env.config.js'
+import userRoutes from '../routes/user.routes.js'
+// import generalRoutes from '../routes/general.routes.js'
 
 const app = express()
 app.use(cors())
@@ -9,5 +11,6 @@ app.use(express.json())
 app.use(morgan(TYPE))
 
 app.use('/users', userRoutes)
+app.use('/', (_req, res) => res.json({ message: 'Welcome to the API' }))
 
 export default app
