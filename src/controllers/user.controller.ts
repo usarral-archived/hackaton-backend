57f1adb1179731606152c446f670b285efee4f67
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-04-13 20:20:15 +0000

    Add Controller for User

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,17 @@
+import User from '../models/user.model'
+
+export const getUsers = async (req, res) => {
+  // TODO: Implement getUsers
+}
+export const getUser = async (req, res) => {
+  // TODO: Implement getUser
+}
+export const createUser = async (req, res) => {
+  // TODO: Implement createUser
+}
+export const updateUser = async (req, res) => {
+  // TODO: Implement updateUser
+}
+export const deleteUser = async (req, res) => {
+  // TODO: Implement deleteUser
+}
