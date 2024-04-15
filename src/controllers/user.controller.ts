b4093738bda3840d6c158ab968d015fe9ad4ef52
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-04-15 16:10:35 +0000

    Hide data sensible and make create and delete user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,13 @@ import { withPrisma } from 'helpers/queryDB.js'
 export const getUsers = async (_req: Request, _res: Response) => {
   withPrisma(async (prisma) => {
     const users = await prisma.user.findMany()
-    _res.status(200).json(users)
+    _res.status(200).json(users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      active: user.active
+
+    })))
   })
 }
 export const getUser = async (_req: Request, _res: Response) => {
@@ -16,16 +22,33 @@ export const getUser = async (_req: Request, _res: Response) => {
     if (user == null) {
       _res.status(404).json({ message: 'User not found' })
     } else {
-      _res.status(200).json(user)
+      _res.status(200).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        active: user.active
+      })
     }
   })
 }
 export const createUser = async (_req: Request, _res: Response) => {
-  // TODO: Implement createUser
+  const { name, email, password } = _req.body
+  withPrisma(async (prisma) => {
+    const user = await prisma.user.create({
+      data: { name, email, password }
+    })
+    _res.status(201).json(user)
+  })
 }
 export const updateUser = async (_req: Request, _res: Response) => {
   // TODO: Implement updateUser
 }
 export const deleteUser = async (_req: Request, _res: Response) => {
-  // TODO: Implement deleteUser
+  const { id } = _req.params
+  withPrisma(async (prisma) => {
+    const user = await prisma.user.delete({
+      where: { id: parseInt(id) }
+    })
+    _res.status(200).json(user)
+  })
 }
