748e29c4c6d9a2f82259deb00bd0b85fed58b027
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-04-18 21:21:57 +0000

    Delete index route

--- api/router/general.routes.ts
+++ api/router/general.routes.ts
@@ -1,7 +1,4 @@
 import { Router } from 'express'
-import { indexRoute } from '@controllers/general.controller.js'
 export const router = Router()
-
-router.use('/', indexRoute)
 
 export default router
